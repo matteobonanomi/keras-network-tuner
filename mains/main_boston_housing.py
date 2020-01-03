5f275b992ab0454b3b3baaa9b5c62ab03591692f
Matteo Bonanomi <matteo.bonanomi@edison.it>
2020-01-03 15:19:22 +0100

    [NEW] Add GridSearch/RandomizedSearch switch option

--- mains/main_boston_housing.py
+++ mains/main_boston_housing.py
@@ -31,18 +31,15 @@ logger.setLevel(logging.INFO)
 with open("../config/keras_config.yaml", 'r') as stream:
     kerasgrid_config = yaml.safe_load(stream)
 
-kerasgrid_config['grid_params']['network'] = convert_yaml2grid(kerasgrid_config['grid_params']['network'])
-
 # PREPROCESSING
 (X_train, y_train), (X_test, y_test) = boston_housing.load_data()
-
 X_scaler = StandardScaler()
 X_scaler.fit(X_train)
 X_train = X_scaler.transform(X_train)
 X_test = X_scaler.transform(X_test)
 
 # TRAIN
-gridKeras = gridSearchKerasNN(kerasgrid_config)
+gridKeras = KerasTuner(kerasgrid_config)
 gridKeras.set_callbacks(generate_callbacks)
 gridKeras.initialize_network(create_model)
 gridKeras.initialize_grid()
--- source/GridSearchKeras.py
+++ source/GridSearchKeras.py
@@ -3,8 +3,8 @@ import pandas as pd
 import os
 import logging
 import yaml
-
-from sklearn.model_selection import GridSearchCV
+from source.utils.auxiliary_functions import *
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from tensorflow.keras.regularizers import *
 from tensorflow.keras.layers import *
@@ -18,74 +18,155 @@ from source.utils.auxiliary_functions import *
 logger = logging.getLogger()
 
 
-class gridSearchKerasNN(object):
+class KerasTuner(object):
+    """
+    Grid Search CV class for Keras-based neural network models
+    """
 
     def __init__(self, config=None):
+        """
+        Initialize class attri
+        :param config:
+        """
+        self._generate_callbacks = return_empty_list
+        self._grid_params = None
+        self._model = None
+        self._cv_params = None
+        self._grid = None
+        self._grid_results = None
+        self._is_model_initialized = False
+        self._is_grid_initialized = False
+        self._is_trained = False
+        self._config = None
 
         if config is None:
             logger.info('No default config dictionaty was loaded')
         else:
             self.set_config_params(config)
-            self._config = config
             logger.info('Default config dictionaty was initialized')
 
-        self._generate_callbacks = return_empty_list
+        return
+
+    def set_config_params(self, config=None):
+        """
+        Initialize configuration parameters
+        Follow sample keras_config.yaml to fill network architecture hyper parameters
+        :param config:
+        :return:
+        """
+
+        if config is None:
+            logger.info('Default config parameters will be used')
+        else:
+            config['grid_params']['network'] = convert_yaml2grid(config['grid_params']['network'])
+            self.set_grid_params(config['grid_params'])
+            self.set_cv_params(config['cv_params'])
+            self._config = config
+            logger.info('Passed config parameters will be used')
 
         return
 
     def set_grid_params(self, params):
+        """
+        Set grid search params to tune neural network hyperparameters:
+        1) Network architecture
+        2) Epochs
+        3) Batch size
+        4) Optimizer
+
+        :param params: dictionary (see keras_config.yaml for details)
+        :return:
+        """
 
         self._grid_params = params
 
         return
 
     def set_callbacks(self, callbacks_function):
-
+        """
+        Set callback function.
+        This function must provide a list of Keras callbacks to be applied when fitting the model
+        Default function returns an empty list (no callbacks applied)
+        :param callbacks_function: function object
+        :return:
+        """
         self._generate_callbacks = callbacks_function
 
         return
 
     def set_cv_params(self, params):
-
+        """
+        Set scikit-learn cross validation paramters:
+        1) n_jobs
+        2) cv
+        3) verbose
+        4) scoring
+        :param params: dictionary (see keras_config.yaml for details)
+        :return:
+        """
         self._cv_params = params
 
         return
 
-    def set_config_params(self, config=None):
-
-        if config is None:
-            logger.info('Default config parameters will be used')
-        else:
-            self.set_grid_params(config['grid_params'])
-            self.set_cv_params(config['cv_params'])
-            logger.info('Passed config parameters will be used')
-
-        return
-
     def initialize_network(self, network_function):
+        """
+        Initialize KerasRegressor object that makes Keras model compatible with scikit-learn library
+        :param network_function: any function that returns a compiled Keras model, taking hyper parameters as input
+        :return:
+        """
 
         self._model = KerasRegressor(build_fn=network_function)
+        self._is_model_initialized = True
 
         return
 
     def initialize_grid(self):
+        """
+        Initialize grid search object, given model function, grid search params and cross validation params
+        Two tuning strategies available:
+        1) Grid Search CV --> cross validation + full grid search
+        2) Randomized Search CV --> cross validation + search on a random combination of hyper parameters
+        Use Randomized Search for fast model training, Grid Search for good model quality
+        :return:
+        """
 
+        assert self._is_model_initialized, 'Keras model was not instantiated (initialize_network method)'
+        assert self._config is not None, 'No config yaml file have been initialized'
+        assert self._cv_params is not None, 'No cross validation params have been initialized'
         _cv = self._cv_params['cv']
         _verb = self._cv_params['verb']
         _scoring = self._cv_params['scoring']
         _n_jobs = self._cv_params['n_jobs']
-
-        self._grid = GridSearchCV(estimator=self._model,
-                                  param_grid=self._grid_params,
-                                  scoring=_scoring,
-                                  cv=_cv,
-                                  verbose=_verb,
-                                  n_jobs=_n_jobs)
-        logger.info('GridSearchCV object is instantiated')
+        _mode = self._config['mode']
+        if _mode == 'grid_search':
+            logger.info('Grid Search mode is selected')
+            self._grid = GridSearchCV(estimator=self._model,
+                                      param_grid=self._grid_params,
+                                      scoring=_scoring,
+                                      cv=_cv,
+                                      verbose=_verb,
+                                      n_jobs=_n_jobs)
+        elif _mode == 'randomized_search':
+            _n_iter = self._config['cv_params']['n_iter']
+            logger.info('Randomized Search mode with {} iterations is selected'.format(_n_iter))
+            self._grid = RandomizedSearchCV(self._model,
+                                            self._grid_params,
+                                            n_iter=_n_iter,
+                                            scoring=_scoring,
+                                            cv=_cv,
+                                            verbose=_verb,
+                                            n_jobs=_n_jobs)
+        else:
+            logger.error('Invalid KerasTuner mode: {}'.format(_mode))
+            raise
+        self._is_grid_initialized = True
+        logger.info('KerasTuner object is instantiated')
 
         return
 
     def fit(self, X_train, y_train, X_test, y_test):
+
+        assert self._is_grid_initialized, 'Grid Search object was not instantiated (initialize_grid method)'
 
         self._grid_params['input_layer_shape'] = [X_train.shape[1]]
 
